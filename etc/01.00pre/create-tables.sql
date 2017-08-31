ee89993ccd908b1513a53d074829eba32d46ada9
Andrew McNab <andrew.mcnab@cern.ch>
2017-08-31 11:06:40 +0100

    Start 01.00pre with Vac 02.01

---
+++ etc/01.00pre/create-tables.sql
@@ -0,0 +1,89 @@
+
+DROP TABLE IF EXISTS `hosts`;
+CREATE TABLE `hosts` (
+  `hostid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `spaceid` mediumint(8) unsigned NOT NULL,
+  `hostname` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `mac` varchar(255) NOT NULL,
+  `siteid` mediumint(8) unsigned NOT NULL,
+  `mb_per_machine` smallint(5) unsigned NOT NULL,
+  `udp_timeout_seconds` float NOT NULL,
+  `total_machines` tinyint(3) unsigned NOT NULL,
+  `cpu_total` tinyint(3) unsigned NOT NULL,
+  `cpu_per_machine` tinyint(3) unsigned NOT NULL,
+  `hs06_per_cpu` float NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`hostid`),
+  UNIQUE KEY `siteid` (`siteid`,`hostname`),
+  UNIQUE KEY `mac` (`mac`,`siteid`),
+  UNIQUE KEY `ip` (`ip`,`siteid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `siteadmins`;
+CREATE TABLE `siteadmins` (
+  `siteid` mediumint(8) unsigned NOT NULL,
+  `dn` varchar(255) NOT NULL,
+  `added` datetime NOT NULL,
+  UNIQUE KEY `siteid` (`siteid`,`dn`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `sites`;
+CREATE TABLE `sites` (
+  `siteid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `sitename` varchar(255) NOT NULL,
+  `p12` blob NOT NULL,
+  `p12updated` datetime NOT NULL,
+  PRIMARY KEY (`siteid`),
+  UNIQUE KEY `sitename` (`sitename`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `spaces`;
+CREATE TABLE `spaces` (
+  `spaceid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `siteid` mediumint(8) unsigned NOT NULL,
+  `spacename` varchar(255) NOT NULL,
+  `published` datetime NOT NULL,
+  `publishlog` text NOT NULL,
+  PRIMARY KEY (`spaceid`),
+  UNIQUE KEY `spacename` (`spacename`,`siteid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `sshkeys`;
+CREATE TABLE `sshkeys` (
+  `keyid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `siteid` mediumint(8) unsigned NOT NULL,
+  `keyvalue` text NOT NULL,
+  `comment` text NOT NULL,
+  `added` datetime NOT NULL,
+  `keytype` varchar(20) NOT NULL DEFAULT 'ssh-rsa',
+  PRIMARY KEY (`keyid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `subnets`;
+CREATE TABLE `subnets` (
+  `subnetid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `spaceid` mediumint(8) unsigned NOT NULL,
+  `subnet` varchar(255) NOT NULL,
+  `netmask` varchar(255) NOT NULL,
+  `router` varchar(255) NOT NULL,
+  `nameservers` varchar(255) NOT NULL,
+  PRIMARY KEY (`subnetid`),
+  UNIQUE KEY `spaceid` (`spaceid`,`subnet`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `vmtypeopts`;
+CREATE TABLE `vmtypeopts` (
+  `vmtypeoptid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `vmtypeid` mediumint(8) unsigned NOT NULL,
+  `optname` varchar(255) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`vmtypeoptid`),
+  UNIQUE KEY `vmtypeid` (`vmtypeid`,`optname`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `vmtypes`;
+CREATE TABLE `vmtypes` (
+  `vmtypeid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `spaceid` mediumint(8) unsigned NOT NULL,
+  `vmtypename` varchar(255) NOT NULL,
+  `p12` blob NOT NULL,
+  `p12updated` datetime NOT NULL,
+  PRIMARY KEY (`vmtypeid`),
+  UNIQUE KEY `spaceid` (`spaceid`,`vmtypename`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
---
+++ etc/01.00pre/viab-conf.spec
@@ -0,0 +1,56 @@
+Name: viab-conf
+Release: ##RPM_RELEASE##
+Version: ##RPM_VERSION##
+BuildArch: noarch
+Summary: Vac-in-a-Box configuration
+License: BSD
+Group: System Environment
+Source: viab-conf.tgz
+URL: https://viab.gridpp.ac.uk/
+Vendor: GridPP
+Packager: Andrew McNab <Andrew.McNab@cern.ch>
+Requires: viab-tftpboot,vac,dhcp,tftp,tftp-server,syslinux-tftpboot,squid,ca-policy-lcg,xauth,tigervnc,ntp,yum-cron,apel-ssm,rpm-build
+
+%description
+Configuration files and idempotent scripts for Vac-in-a-Box
+
+%prep
+
+%setup -n viab-conf
+
+%build
+
+%install
+mkdir -p $RPM_BUILD_ROOT/usr/sbin \
+         $RPM_BUILD_ROOT/etc/viab \
+         $RPM_BUILD_ROOT/etc/vac.d \
+         $RPM_BUILD_ROOT/etc/rc.d/init.d \
+         $RPM_BUILD_ROOT/root/.ssh \
+         $RPM_BUILD_ROOT/etc/squid \
+         $RPM_BUILD_ROOT/etc/squid \
+         $RPM_BUILD_ROOT/etc/yum.repos.d \
+         $RPM_BUILD_ROOT/var/lib/vac/machinetypes
+
+cp -p viab-conf-postinstall viab-conf-p12 lazyssh dnsmasq-wrapper \
+ viab-heartbeat viab-conf-iptables $RPM_BUILD_ROOT/usr/sbin
+cp viab-iptables.init $RPM_BUILD_ROOT/etc/rc.d/init.d/viab-iptables
+cp vac.d/*.conf $RPM_BUILD_ROOT/etc/vac.d
+cp viab/* $RPM_BUILD_ROOT/etc/viab
+cp squid.conf.template $RPM_BUILD_ROOT/etc/squid/squid.conf.template
+cp -p authorized_keys $RPM_BUILD_ROOT/root/.ssh
+cp -a machinetypes/* $RPM_BUILD_ROOT/var/lib/vac/machinetypes
+
+%pre
+rm -f /etc/vac.d/*
+
+%posttrans
+/usr/sbin/viab-conf-postinstall
+
+%files
+/etc/rc.d/init.d/*
+/etc/vac.d/*
+/etc/viab/*
+/etc/squid/*
+/var/lib/vac/machinetypes/*
+/usr/sbin/*
+/root/.ssh/authorized_keys
